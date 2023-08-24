d81700ffe5be884dde6c5a9ab36ded6c3c5e6342
Xander <xtilk2@huskers.unl.edu>
2023-08-24 12:58:57 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Xander Tilk partner group 16
+ *         xtilk2@unl.edu
+ * Date: 2023-8-24
+ *
+ * Program that says Hello Xander
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Xander\n");
+
+}
