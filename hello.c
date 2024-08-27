73b80f009da5d46194259a800e230c949493a3af
Globnefario <xtilk2@huskers.unl.edu>
2024-08-27 13:14:29 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Xander Tilk
+ * Date: 2024/08/27
+ *
+ * A simple Hello [Name] program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello Xander\n");
+
+    return 0;
+}
